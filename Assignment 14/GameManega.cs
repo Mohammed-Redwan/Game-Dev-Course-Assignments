1fdf3a0b67543ae13cc4c9ddee770c0460753058
Mohammed Redwan <rdadwan93@gmail.com>
2025-07-23 11:41:58 +0300

    assignment 14 added

---
+++ Assignment 14/GameManega.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ass14 {
+    public class GameManega : MonoBehaviour
+    {
+        Player14 p = new Player14("mohammed", 100);
+        Enemy e = new Enemy("shlomo", 23);
+        Debug.Log(23);
+    }
+}
